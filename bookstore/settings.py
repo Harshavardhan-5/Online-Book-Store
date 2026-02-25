41ceb63007d6c23b6476136c77178684aced4cda
Harshavardhan Khambayat <166542187+Harshavardhan-5@users.noreply.github.com>
2026-02-25 16:58:13 +0530

    Update settings.py

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -25,7 +25,7 @@ import os
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['web-production-0c026e.up.railway.app']
 
