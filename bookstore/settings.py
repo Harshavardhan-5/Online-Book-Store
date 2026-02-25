31dbe2ec29cc117050d09cff8bad8102da296f04
Harshavardhan-5 <harshvardhankhambayat24@gmail.com>
2026-02-25 20:06:54 +0530

    Removed auto superuser code

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -180,15 +180,3 @@ ALLOWED_HOSTS = [
 ]
 
 
-import os
-
-if os.environ.get("CREATE_SUPERUSER") == "True":
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser(
-            username="admin",
-            email="admin@gmail.com",
-            password="admin123"
-        )
